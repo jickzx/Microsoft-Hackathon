a5058d3ea652b38c6208c967864adde868f4959d
Ayush Gupta <ayushg0500@gmail.com>
2026-05-11 12:46:09 +0100

    update

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ["dist", ".playwright-cli"] },
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -26,7 +28,9 @@ export default [
         "warn",
         { allowConstantExport: true }
       ],
-      "no-unused-vars": "off"
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   },
   {
